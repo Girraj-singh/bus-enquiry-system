c76fb40d88fa274ae0cf9f0340ddd6ef9f60baf4
Girraj-singh <85035083+Girraj-singh@users.noreply.github.com>
2022-11-04 22:36:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        mobile: '500px'
+      }
+    },
+  },
+  plugins: [],
+}
